f5afc01f8fb71c42231459706a149b9e317b1892
kdw <ksgkdw78@naver.com>
2025-02-02 22:05:14 +0900

    stock_list

--- src/component/stock_list/StockList.jsx
+++ src/component/stock_list/StockList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaPlusCircle } from "react-icons/fa";
-
+import { IoFilter } from "react-icons/io5";
 import "../../assets/css/stock/stock_list.css";
 
 const StockList = () => {
@@ -29,46 +29,65 @@ const StockList = () => {
         <div className="stockPin_create_filter_box">
           <h3>StockPin 추천 필터</h3>
           <ul>
-            <li className="active">
-              <h4>연속 상승세</h4>
+            <li>
+              <h4 className="active">연속 상승세</h4>
               <div className="hover_content_box">
-                <span>연속 상승세</span>
-                <p>일주일 연속 상승세를 보이는 주식</p>
+                <div>
+                  <span>연속 상승세</span>
+                  <p>일주일 연속 상승세를 보이는 주식</p>
+                </div>
               </div>
             </li>
             <li>
               <h4>꾸준한 배당주</h4>
               <div className="hover_content_box">
-                <span>꾸준한 배당주</span>
-                <p>배당을 꾸준히 하는 주식</p>
+                <div>
+                  <span>꾸준한 배당주</span>
+                  <p>배당을 꾸준히 하는 주식</p>
+                </div>
               </div>
             </li>
             <li>
               <h4>돈 잘 버는 회사</h4>
               <div className="hover_content_box">
-                <span>돈 잘 버는 회사</span>
-                <p>매출과 이익을 크게 내는 회사</p>
+                <div>
+                  <span>돈 잘 버는 회사</span>
+                  <p>매출과 이익을 크게 내는 회사</p>
+                </div>
               </div>
             </li>
             <li>
               <h4>안정 성장주</h4>
               <div className="hover_content_box">
-                <span>안정 성장주</span>
-                <p>꾸준하게 이익이 늘고 있는 회사</p>
+                <div>
+                  <span>안정 성장주</span>
+                  <p>꾸준하게 이익이 늘고 있는 회사</p>
+                </div>
               </div>
             </li>
           </ul>
         </div>
       </aside>
       <main>
-        <div>
-          <h3>해외 주식</h3>
-          <p>해외에 포함되는 주식</p>
+        <div className="filter_title_container">
+          <h3>연속 상승세</h3>
+          <p>일주일 연속 상승세를 보이는 주식</p>
         </div>
-        <nav>
+        <nav className="filter_list_container">
           <ul>
-            <li>필터 추가</li>
-            <li>국내 해외</li>
+            <li>
+              <button>
+                <IoFilter />
+                <span>필터 추가</span>
+              </button>
+              <div></div>
+            </li>
+            <li>
+              <button>
+                <span>국내</span>
+              </button>
+              <div></div>
+            </li>
             <li>카테고리</li>
             <li>시가총액</li>
             <li>거래량</li>
