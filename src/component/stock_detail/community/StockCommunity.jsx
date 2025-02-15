ac4d56b4307301ee2029b0af992214744157ea2e
kdw <ksgkdw78@naver.com>
2025-02-15 15:19:49 +0900

    stock detail : stock community

--- src/component/stock_detail/community/StockCommunity.jsx
+++ src/component/stock_detail/community/StockCommunity.jsx
@@ -1,51 +1,62 @@
 import React from "react";
+import "@assets/css/stock_detail/community/stockCommunity.css";
 import StockUserComment from "./StockUserComment";
-
+import { FaUserCircle } from "react-icons/fa";
+import SortIcon from "@assets/images/sort_icon.svg";
 const StockCommunity = () => {
   return (
-    <div>
-      <div>
-        <h3>삼성전자 커뮤니티</h3>
-        <div>
-          <div></div>
-          <div>
-            <textarea
-              name=""
-              id=""
-              value={"의견을 남기려면 프로필 로그인이 필요해요"}
-            ></textarea>
-          </div>
-        </div>
-        <div>
-          <div className="sort">
-            <button>인기순</button>
-            <button>최신순</button>
-          </div>
-          <div>
-            <StockUserComment />
-            <div className="replay">
-              <StockUserComment />
+    <div className="stock_community_wrap">
+      <div className="stock_community_container">
+        <div className="content">
+          <h3>삼성전자 커뮤니티</h3>
+          <div className="user_comment_container">
+            <div className="user_profile_box">
+              <FaUserCircle />
             </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <h4>인기 급상승 커뮤니티</h4>
-        <ul>
-          <li>
-            <div>
-              <span></span>
-              <div></div>
-              <div>
-                <div>
-                  <span></span>
-                  <span></span>
-                </div>
-                <span></span>
+            <div className="user_comment_box">
+              <textarea
+                name=""
+                id=""
+                placeholder="***님의 의견을 남겨주세요"
+              ></textarea>
+              <div className="user_comment_btn_box">
+                <button>등록</button>
               </div>
             </div>
-          </li>
-        </ul>
+          </div>
+          <div>
+            <div className="sort_btn_box">
+              <button>
+                <span>인기순</span>
+                <SortIcon />
+              </button>
+            </div>
+            <div>
+              <StockUserComment />
+              <div className="replay">
+                <StockUserComment />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="ranking">
+          <h4>인기 급상승 커뮤니티</h4>
+          <ul>
+            <li>
+              <div>
+                <span></span>
+                <div></div>
+                <div>
+                  <div>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <span></span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/component/stock_detail/info/StockInfo.jsx
+++ src/component/stock_detail/info/StockInfo.jsx
@@ -2,67 +2,69 @@ import React, { useState } from "react";
 import style from "@assets/css/stock_detail/info/stockInfo.module.css";
 const StockInfo = () => {
   return (
-    <div className={style.stock_info_container}>
-      <nav>
-        <ul>
-          <li className={style.active}>주요 정보</li>
-          <li>재무</li>
-          <li>실적</li>
-          <li>배당</li>
-        </ul>
-      </nav>
-      <div className={style.content}>
-        <div className={style.title}>
-          <div className={style.stock_name_conatainer}>
-            <h2>삼성전자</h2>
-            <span>국내・005930・코스피</span>
-          </div>
-          <p>출처: api</p>
-        </div>
-        <div className={style.stock_information_line_container}>
-          <p>
-            한국 및 DX부문 해외 9개 지역총괄과 DS부문 해외 5개 지역총괄, SDC,
-            Harman 등 229개의 종속기업으로 구성된 글로벌 전자기업임.
-          </p>
-        </div>
-        <div className={style.stock_main_info_contianer}>
-          <div className={style.item}>
-            <span>시가총액</span>
-            <span>369조 9,993억원</span>
-          </div>
-          <div className={style.item}>
-            <span>실제 기업 가치</span>
-            <span>322조 1,819억원</span>
-          </div>
-          <div className={style.item}>
-            <span>기업명</span>
-            <span>SamsungElectronics</span>
-          </div>
-          <div className={style.item}>
-            <span>대표이사</span>
-            <span>한종희</span>
-          </div>
-          <div className={style.item}>
-            <span>상장일</span>
-            <div className={style.line_break_box}>
-              <span>1975년 6월 11일</span>
-              <span className={style.sub}>1969년 설립</span>
+    <div className={style.stock_info_wrap}>
+      <div className={style.stock_info_container}>
+        <nav>
+          <ul>
+            <li className={style.active}>주요 정보</li>
+            <li>재무</li>
+            <li>실적</li>
+            <li>배당</li>
+          </ul>
+        </nav>
+        <div className={style.content}>
+          <div className={style.title}>
+            <div className={style.stock_name_conatainer}>
+              <h2>삼성전자</h2>
+              <span>국내・005930・코스피</span>
             </div>
+            <p>출처: api</p>
           </div>
-          <div className={style.item}>
-            <span>발행주식수</span>
-            <div className={style.line_break_box}>
-              <span>6,792,669,250주</span>
-              <span className={style.sub}>25년 2월 13일 기준</span>
+          <div className={style.stock_information_line_container}>
+            <p>
+              한국 및 DX부문 해외 9개 지역총괄과 DS부문 해외 5개 지역총괄, SDC,
+              Harman 등 229개의 종속기업으로 구성된 글로벌 전자기업임.
+            </p>
+          </div>
+          <div className={style.stock_main_info_contianer}>
+            <div className={style.item}>
+              <span>시가총액</span>
+              <span>369조 9,993억원</span>
             </div>
-          </div>
-          <div className={style.item}>
-            <span>홈페이지</span>
-            <span>
-              <a target="_blink" href="https://www.samsung.com/sec/">
-                https://www.samsung.com/sec
-              </a>
-            </span>
+            <div className={style.item}>
+              <span>실제 기업 가치</span>
+              <span>322조 1,819억원</span>
+            </div>
+            <div className={style.item}>
+              <span>기업명</span>
+              <span>SamsungElectronics</span>
+            </div>
+            <div className={style.item}>
+              <span>대표이사</span>
+              <span>한종희</span>
+            </div>
+            <div className={style.item}>
+              <span>상장일</span>
+              <div className={style.line_break_box}>
+                <span>1975년 6월 11일</span>
+                <span className={style.sub}>1969년 설립</span>
+              </div>
+            </div>
+            <div className={style.item}>
+              <span>발행주식수</span>
+              <div className={style.line_break_box}>
+                <span>6,792,669,250주</span>
+                <span className={style.sub}>25년 2월 13일 기준</span>
+              </div>
+            </div>
+            <div className={style.item}>
+              <span>홈페이지</span>
+              <span>
+                <a target="_blink" href="https://www.samsung.com/sec/">
+                  https://www.samsung.com/sec
+                </a>
+              </span>
+            </div>
           </div>
         </div>
       </div>
