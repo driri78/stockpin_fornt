cc6982f7e65799fe666ab7c47fc378fd1f557b97
kdw <ksgkdw78@naver.com>
2025-04-09 22:16:22 +0900

    sign checkbox all fnc

--- src/component/auth/AgreementCheckbox.jsx
+++ src/component/auth/AgreementCheckbox.jsx
@@ -1,15 +1,41 @@
 import { Check } from "@/assets/images/Image";
-import React from "react";
+import React, { useState } from "react";
 
 const AgreementCheckbox = ({ data }) => {
+  const [checkedItems, setCheckedItems] = useState(() => {
+    const initChecked = {};
+    data.forEach((item) => {
+      initChecked[item.id] = false;
+    });
+    return initChecked;
+  });
+
+  const handleCheck = (id) => {
+    let newState = {};
+    let checked = !checkedItems[id];
+    if (id === "all") {
+      data.forEach((item) => {
+        newState[item.id] = checked;
+      });
+    } else {
+      newState = { ...checkedItems };
+      newState[id] = checked;
+    }
+    setCheckedItems(newState);
+  };
   return (
     <>
       {data.map((item) => (
-        <div className="item">
+        <div className="item" key={item.id}>
           <label className="check_img_box" htmlFor={item.id}>
             <Check />
           </label>
-          <input type="checkbox" id={item.id} />
+          <input
+            type="checkbox"
+            id={item.id}
+            checked={checkedItems[item.id]}
+            onChange={() => handleCheck(item.id)}
+          />
           <label htmlFor={item.id}>{item.text}</label>
         </div>
       ))}
