342abb2682636f2a1d404555d5f80583682feb2d
kdw <ksgkdw78@naver.com>
2025-04-10 22:32:37 +0900

    email select show

--- src/component/auth/inputBox/Email.jsx
+++ src/component/auth/inputBox/Email.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 
 const Email = () => {
+  const [select, setSelect] = useState(false);
+
   return (
     <div className="email_box">
       <div className="input_box">
@@ -10,10 +12,13 @@ const Email = () => {
       <div>
         <span>@</span>
       </div>
-      <div className="select_box essential_input">
+      <div
+        className="select_box essential_input"
+        onClick={() => setSelect(!select)}
+      >
         <span>선택</span>
         <IoIosArrowDown />
-        <ul className="">
+        <ul className={select ? "active" : ""}>
           <li>naver.com</li>
           <li>gmail.com</li>
           <li>daum.net</li>
