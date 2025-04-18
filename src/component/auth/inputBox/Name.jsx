701735a73fdcf2cfdc416bfa0096a3ad3a5e7a3f
kdw <ksgkdw78@naver.com>
2025-04-18 23:13:11 +0900

    name valid

--- src/component/auth/inputBox/Name.jsx
+++ src/component/auth/inputBox/Name.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Name = () => {
+  const [name, setName] = useState("");
+  const validName = (e) => {
+    let input = e.target.value;
+    if (input.length <= 10) {
+      setName(input);
+    }
+  };
+
   return (
     <div className="name_box">
-      <input className="essential_input" type="text" placeholder="이름" />
+      <input
+        className="essential_input"
+        type="text"
+        placeholder="이름"
+        value={name}
+        onChange={validName}
+      />
     </div>
   );
 };
