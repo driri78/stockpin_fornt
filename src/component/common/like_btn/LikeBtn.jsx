a88bdc37397d365f7e680fba1f9feeb4fe80efad
kdw <ksgkdw78@naver.com>
2025-04-27 22:55:35 +0900

    like btn

--- src/component/common/like_btn/LikeBtn.jsx
+++ src/component/common/like_btn/LikeBtn.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHeart } from "react-icons/fa";
 
 const LikeBtn = () => {
+  const [like, setlike] = useState(false);
   return (
-    <div className="like_btn_img_box">
-      <FaHeart style={{ color: "var(--primary-color)" }} />
+    <div
+      className={`like_btn_img_box ${like ? "active" : ""}`}
+      onClick={(e) => setlike(!like)}
+    >
+      <FaHeart />
     </div>
   );
 };
