59e80fced55e72c4d0e211477f72653322ad9ec5
kdw <ksgkdw78@naver.com>
2025-05-07 23:39:52 +0900

    trending paging arrow step

--- src/component/home/section01/trending_stock/Paging.jsx
+++ src/component/home/section01/trending_stock/Paging.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 const Paging = ({ first, last, currentPage, setCurrentPage }) => {
   const pageArr = Array.from({ length: last - first + 1 }, (v, i) => first + i);
-  console.log(first);
+
+  const arrowHandle = (pageStep) => {
+    const nextPage = currentPage + pageStep;
+    if (pageArr[0] <= nextPage && nextPage <= pageArr[pageArr.length - 1]) {
+      setCurrentPage(nextPage);
+    }
+  };
+
   return (
     <nav className="paging_container">
       <ul>
-        <li className="img_box">
+        <li className="img_box" onClick={() => arrowHandle(-1)}>
           <FaAngleLeft />
         </li>
         {pageArr.map((page) => (
@@ -18,7 +25,7 @@ const Paging = ({ first, last, currentPage, setCurrentPage }) => {
             {page}
           </li>
         ))}
-        <li className="img_box">
+        <li className="img_box" onClick={() => arrowHandle(1)}>
           <FaAngleRight />
         </li>
       </ul>
