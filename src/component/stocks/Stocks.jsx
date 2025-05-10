0cf74d183aac299d1f85fb0ddcd7cf7b2debd7f3
kdw <ksgkdw78@naver.com>
2025-05-10 23:42:29 +0900

    stock filter list

--- src/component/stocks/Stocks.jsx
+++ src/component/stocks/Stocks.jsx
@@ -9,6 +9,7 @@ import "@assets/css/stocks/stocks.css";
 import { KoreaFlag } from "@/assets/images/Image";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import StockFilterList from "./StockFilterList";
 const Stocks = () => {
   const navigate = useNavigate();
   const goDetail = (id) => {
@@ -53,47 +54,7 @@ const Stocks = () => {
             </li>
           </ul>
         </div>
-        <div className="stockPin_create_filter_box">
-          <h3>StockPin 추천 필터</h3>
-          <ul>
-            <li>
-              <h4 className="active">연속 상승세</h4>
-              <div className="hover_content_box">
-                <div>
-                  <span>연속 상승세</span>
-                  <p>일주일 연속 상승세를 보이는 주식</p>
-                </div>
-              </div>
-            </li>
-            <li>
-              <h4>꾸준한 배당주</h4>
-              <div className="hover_content_box">
-                <div>
-                  <span>꾸준한 배당주</span>
-                  <p>배당을 꾸준히 하는 주식</p>
-                </div>
-              </div>
-            </li>
-            <li>
-              <h4>돈 잘 버는 회사</h4>
-              <div className="hover_content_box">
-                <div>
-                  <span>돈 잘 버는 회사</span>
-                  <p>매출과 이익을 크게 내는 회사</p>
-                </div>
-              </div>
-            </li>
-            <li>
-              <h4>안정 성장주</h4>
-              <div className="hover_content_box">
-                <div>
-                  <span>안정 성장주</span>
-                  <p>꾸준하게 이익이 늘고 있는 회사</p>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
+        <StockFilterList />
       </aside>
       <main>
         <div className="filter_title_container">
